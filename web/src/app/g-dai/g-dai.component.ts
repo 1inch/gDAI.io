a5dc9d39d22359be0c2d3f0282b7ad17b5a1dd9d
deacix <info@deacix.de>
2019-09-07 11:41:24 -0400

    added card and loading spinner

--- web/src/app/g-dai/g-dai.component.ts
+++ web/src/app/g-dai/g-dai.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-g-dai',
-  templateUrl: './g-dai.component.html',
-  styleUrls: ['./g-dai.component.scss']
+    selector: 'app-g-dai',
+    templateUrl: './g-dai.component.html',
+    styleUrls: ['./g-dai.component.scss']
 })
 export class GDaiComponent implements OnInit {
 
-  constructor() { }
+    loading = true;
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
 
+    ngOnInit() {
+    }
 }
